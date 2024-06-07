18af362743d9ef7e5d209cebf9a0ef7c2b1f1799
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-08 00:49:31 +0100

    for each

--- Vanila.js
+++ Vanila.js
@@ -1,45 +1,11 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  function ask(question) {
-    return new Promise((resolve) => readline.question(question, (answer) => resolve(answer)));
-  }
-  
-  async function main() {
-    const num1 = parseFloat(await ask("Enter first number: "));
-    const operator = await ask("Enter operator (+, -, *, /): ");
-    const num2 = parseFloat(await ask("Enter second number: "));
-  
-    let result;
-    switch (operator) {
-      case "+":
-        result = num1 + num2;
-        break;
-      case "-":
-        result = num1 - num2;
-        break;
-      case "*":
-        result = num1 * num2;
-        break;
-      case "/":
-        if (num2 === 0) {
-          console.error("Error: Cannot divide by zero");
-        } else {
-          result = num1 / num2;
-        }
-        break;
-      default:
-        console.error("Error: Invalid operator");
-    }
-  
-    if (result !== undefined) {
-      console.log("Result:", result);
-    }
-  
-    readline.close();
-  }
-  
-  main();
-  
+const fruits = ["apples", "bananas", "cherries"];
+
+const num = [1,2,3];
+
+for (const i of fruits) {
+  console.log(i);
+};
+
+for (const x of num) {
+  console.log(x);
+};
