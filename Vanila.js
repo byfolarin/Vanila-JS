f104fa5dbbc909c2ec07ea6aba1b94b437df8181
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-18 09:14:13 +0100

    vanilla data call

--- Vanila.js
+++ Vanila.js
@@ -9,3 +9,15 @@ for (const i of fruits) {
 for (const x of num) {
   console.log(x);
 };
+
+
+function greet(name, callback) {
+  console.log('Hello, ' + name);
+  callback();
+}
+
+function sayGoodbye() {
+  console.log('Goodbye!');
+}
+
+greet('Alice', sayGoodbye);
