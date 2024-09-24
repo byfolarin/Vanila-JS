4163b7b2adf7f151508f2f3e3a1bf60ef220e6ec
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-24 11:12:34 +0100

    style

--- Vanila.js
+++ Vanila.js
@@ -1,23 +1,60 @@
-const fruits = ["apples", "bananas", "cherries"];
+window.onload = function () {
+  
+  let seconds = "00"; 
+  let tens = "00"; 
+  let appendTens = document.getElementById("tens")
+  let appendSeconds = document.getElementById("seconds")
+  let buttonStart = document.getElementById('button-start');
+  let buttonStop = document.getElementById('button-stop');
+  let buttonReset = document.getElementById('button-reset');
+  let Interval ;
 
-const num = [1,2,3];
+  buttonStart.onclick = function() {
+    
+    clearInterval(Interval);
+     Interval = setInterval(startTimer, 10);
+  }
+  
+    buttonStop.onclick = function() {
+       clearInterval(Interval);
+  }
+  
 
-for (const i of fruits) {
-  console.log(i);
-};
+  buttonReset.onclick = function() {
+     clearInterval(Interval);
+    tens = "00";
+  	seconds = "00";
+    appendTens.innerHTML = tens;
+  	appendSeconds.innerHTML = seconds;
+  }
+  
+   
+  
+  function startTimer () {
+    tens++; 
+    
+    if(tens <= 9){
+      appendTens.innerHTML = "0" + tens;
+    }
+    
+    if (tens > 9){
+      appendTens.innerHTML = tens;
+      
+    } 
+    
+    if (tens > 99) {
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    }
+    
+    if (seconds > 9){
+      appendSeconds.innerHTML = seconds;
+    }
+  
+  }
+  
 
-for (const x of num) {
-  console.log(x);
-};
-
-
-function greet(name, callback) {
-  console.log('Hello, ' + name);
-  callback();
 }
-
-function sayGoodbye() {
-  console.log('Goodbye!');
-}
-
-greet('Alice', sayGoodbye);
