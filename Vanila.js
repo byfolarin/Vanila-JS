81a4ba479998dcecb5b1e15604308659c62873fe
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-24 15:48:08 +0100

    timer

--- Vanila.js
+++ Vanila.js
@@ -7,24 +7,35 @@ window.onload = function() {
   let tens = document.getElementById("tens")
   let Interval;
 
+
   let tensTime = 0;
-  let secondsTime = 0;  
+  let secondsTime = 0;
+  let timer;
 
+  
+   function startTime() {
+    tensTime++;
+    
+    if (tensTime > 99) {
+      secondsTime++;
+      tensTime = 0;
+      seconds.innerHTML = secondsTime < 10 ? "0" + secondsTime : secondsTime;
+    }
+    
+    tens.innerHTML = tensTime < 10 ? "0" + tensTime : tensTime;
+  }
+  
 
+  buttonStart.addEventListener("click", () => {
+    if (!timer) {
+      timer = setInterval(startTime, 10);
+    }
+  });
  
-
-    buttonStart.addEventListener("click", ()=>{
-      let tensTime = 0;
-      let secondsTime = 0;  
-      seconds.innerHTML = secondsTime
-      tens.innerHTML =tensTime
-    })
-
- 
-
-      buttonStop.addEventListener("click", ()=>{
-        clearInterval(Interval)
-      })
+  buttonStop.addEventListener("click", () => {
+    clearInterval(timer);
+    timer = null;
+  });
 
     
       buttonReset.addEventListener("click", ()=>{
@@ -35,10 +46,7 @@ window.onload = function() {
       })
 
 
-    function startTime (){
-      tensTime++
-      
-    }
+   
 
 
       
