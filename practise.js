28b2503e0a9b8a14873a242fd28fab122c8e7663
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-25 17:26:17 +0100

    titles

--- practise.js
+++ practise.js
@@ -3,11 +3,18 @@ function getBooks() {
     fetch('books.json')
         .then(response => response.json())
         .then(data => {
-            console.log(data.id)
+            // Log all book titles
+            data.books.forEach(book => {
+                const bookTitle = book
+                
+            });
+              const bookTitles = `<p>${bookTitle}</p>`
+            document.getElementById('bookTitle').innerHTML=bookTitles
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
 
-console.log(getBooks)
+// Call the function
+getBooks();
